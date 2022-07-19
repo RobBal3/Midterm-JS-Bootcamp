7236126c1a592513329b579b5124391d29010da4
robbal <robert.baloyan@gmail.com>
2022-07-19 16:29:19 +0400

    Free Time Coding - Outside Exam Time

--- Problem_2.js
+++ Problem_2.js
@@ -1,43 +1,40 @@
 // 2. Create a constructor function to produce calculator instances (+,-,*,/ operations).
 
-function Calculator(par1, par2, operator) {
-  this.par1 = par1;
-  this.par2 = par2;
-  this.operator = operator;
-
-  this.add = function () {
-    return this.par1 + this.par2;
+function Calculator() {
+  let add = function (par1, par2) {
+    return par1 + par2;
   };
 
-  this.subtract = function () {
-    return this.par1 - this.par2;
+  let subtract = function (par1, par2) {
+    return par1 - par2;
   };
 
-  this.multiply = function () {
-    return this.par1 * this.par2;
+  let multiply = function (par1, par2) {
+    return par1 * par2;
   };
 
-  this.divide = function () {
-    return this.par1 / this.par2;
+  let divide = function (par1, par2) {
+    return par1 / par2;
   };
 
-  if (operator === "+") {
-    return this.add();
-  }
+  return function (par1, par2, operator) {
+    if (operator === "+") {
+      return add(par1, par2);
+    }
+    if (operator === "-") {
+      return subtract(par1, par2);
+    }
+    if (operator === "*") {
+      return multiply(par1, par2);
+    }
 
-  if (operator === "-") {
-    return this.subtract();
-  }
-
-  if (operator === "*") {
-    return this.multiply();
-  }
-
-  if (operator === "/") {
-    return this.divide();
-  }
+    if (operator === "/") {
+      return divide(par1, par2);
+    }
+  };
 }
 
-let calc = new Calculator(1, 2, "+");
+let calc = new Calculator();
+calc(6, 2, "+");
 // calc(1, 2, "+"); // => 3;
 // calc(3, 4, "-"); //=> -1
--- Problem_4.js
+++ Problem_4.js
@@ -1,5 +1,4 @@
 // Group by property
-
 let arr = [
   { name: "Alice", job: "Data Analyst", country: "AU" },
   { name: "Bob", job: "Pilot", country: "US" },
@@ -9,14 +8,43 @@ let arr = [
   { name: "Jeremy", job: "Artist", country: "SP" },
 ];
 
+// Version 2
 function group(arr, prop) {
-  let groupedObj = {};
+  let obj = {};
 
-  arr.map((item) => {
-    groupedObj[item[prop]] = arr.filter((item1) => {});
+  arr.forEach((item) => {
+    if (!obj[item[prop]]) {
+      obj[item[prop]] = undefined;
+    }
   });
 
-  return groupedObj;
+  for (let key in obj) {
+    obj[key] = arr.filter((item) => {
+      return item[prop] === key;
+    });
+  }
+
+  return obj;
 }
 
+// Version 1
+// function group(arr, prop) {
+//   let groupedObj = {};
+
+//   arr.map((item) => {
+//     groupedObj[item[prop]] = arr.filter((el) => {
+//       return el[prop] === item[prop];
+//     });
+//   });
+
+//   return groupedObj;
+// }
+
 console.log(group(arr, "country"));
+
+//result => {
+// 	AU: [{name: 'Alice', job: 'Data Analyst', country: 'AU'}],
+// 	US: [{name: 'Bob', job: 'Pilot', country: 'US'},
+//   {name: 'Lewis', job: 'Pilot', country: 'US'}],
+// ……
+// }
